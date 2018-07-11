bae973aa9d6f19a0a1072ec0655a8e22372c405f
Quentin <qmalguy@student.42.fr>
2018-07-11 23:25:38 +0200

    clean Utm Class

--- src/index.js
+++ src/index.js
@@ -1,51 +1,6 @@
 export class Utm {
 
-  constructor(url) {
-    this.url = url;
-  }
-
-  get() {
-    if(this.url.length === 0) {
-      throw {
-        name: "Error",
-        message: "Please, insert a valid url"
-      }
-    }
-
-    if(!this.url.includes('?')) {
-      return this.extractParamsFromQueryString();
-    }
-
-    if(this.url.includes('?')) {
-      this.url = this.url.replace('?', '&');
-      return this.get();
-    }
-
-    if(this.url.charAt(0) === '?' || this.url.indexOf('?') > 0) {
-      this.removeValueBeforeParameters();
-      return this.get();
-    }
-  }
-
-
-  extractParamsFromQueryString() {
-
-    let queryObject = Object.create({});
-    const arrayOfParameters = this.url.split('&');
-
-    arrayOfParameters.forEach((e) => {
-      if(e.includes('=')) {
-        const pair = [...e.split('=')];
-        if (pair[0].length > 0 && pair[1].length > 0) {
-          queryObject[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || undefined)
-        }
-      }
-    });
-
-    return Utm.removeOthersParametersThanUtm(queryObject);
-  }
-
-  removeValueBeforeParameters() {
+  static removeValueBeforeParameters() {
     return this.url = this.url.substr(this.url.indexOf('?')).replace('?', '&');
   }
 
@@ -69,5 +24,49 @@ export class Utm {
       }
     }
     return object;
+  }
+
+  constructor(url) {
+    this.url = url;
+  }
+
+  extractParamsFromQueryString() {
+
+    let queryObject = Object.create({});
+    const arrayOfParameters = this.url.split('&');
+
+    arrayOfParameters.forEach((e) => {
+      if(e.includes('=')) {
+        const pair = [...e.split('=')];
+        if (pair[0].length > 0 && pair[1].length > 0) {
+          queryObject[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || undefined)
+        }
+      }
+    });
+
+    return Utm.removeOthersParametersThanUtm(queryObject);
+  }
+
+  get() {
+    if(this.url.length === 0) {
+      throw {
+        name: "Error",
+        message: "Please, insert a valid url"
+      }
+    }
+
+    if(!this.url.includes('?')) {
+      return this.extractParamsFromQueryString();
+    }
+
+    if(this.url.includes('?')) {
+      this.url = this.url.replace('?', '&');
+      return this.get();
+    }
+
+    if(this.url.charAt(0) === '?' || this.url.indexOf('?') > 0) {
+      this.removeValueBeforeParameters();
+      return this.get();
+    }
   }
 }
